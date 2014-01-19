1ad449a89ba88a3a43bded388faaa6cb1ac36e14
Sandro Tosi <morph@debian.org>
2014-01-19 17:32:15 +0100

    improve the template provided to less-expert reporters to be clear the questions should be answered by the reporter themselves; thanks to Tony Houghton for the report; Closes: #690782

--- reportbug/utils.py
+++ reportbug/utils.py
@@ -74,7 +74,8 @@ import ui.text_ui as ui
 from reportbug.ui import AVAILABLE_UIS
 
 NEWBIELINE = """Dear Maintainer,
-*** Please consider answering these questions, where appropriate ***
+
+*** Reporter, please consider answering these questions, where appropriate ***
 
    * What led up to the situation?
    * What exactly did you do (or not do) that was effective (or
@@ -82,7 +83,7 @@ NEWBIELINE = """Dear Maintainer,
    * What was the outcome of this action?
    * What outcome did you expect instead?
 
-*** End of the template - remove these lines ***"""
+*** End of the template - remove these template lines ***"""
 
 fhs_directories = ['/', '/usr', '/usr/share', '/var', '/usr/X11R6',
                    '/usr/man', '/usr/doc', '/usr/bin']
