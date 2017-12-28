kaymccormick/reportbug

commit d7e9eb8cc5d2efa4543bd35a806cbc36d7419e5e

Finish open_url return type conversion
The return type of reportbug/urlutils/open_url() was changed in
commit 5cf006943001a94bfafe0a40adbb1241950853de
from http.client.HTTPResponse to string. A few remaining open_url()
callers have not yet been updated, leading to breakage such as
https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=859274

This commit updates the remaining callers to deal with strings, and
updates a few places to iterate over lines rather than characters.

reportbug/debbugs.py
@@ -968,15 +968,9 @@ def parse_html_report(number, url, http_proxy, timeout, followups=False, cgi=Tru
         return None
 
     parser = BTSParser(cgi=cgi, followups=followups)
-    for line in page:
+    for line in page.splitlines():
         parser.feed(line)
     parser.close()
-
-    try:
-        page.fp._sock.recv = None
-    except:
-        pass
-    page.close()
 
     items = parser.preblock
     title = "#%d: %s" % (number, parser.title)
@@ -1012,13 +1006,7 @@ def parse_mbox_report(number, url, http_proxy, timeout, followups=False):
         return None
 
     # Make this seekable
-    wholefile = io.StringIO(page.read())
-
-    try:
-        page.fp._sock.recv = None
-    except:
-        pass
-    page.close()
+    wholefile = io.StringIO(page)
 
     mbox = mailbox.UnixMailbox(wholefile, msgfactory)
     title = ''
@@ -1132,14 +1120,9 @@ def get_reports(package, timeout, system='debian', mirrors=None, version=None,
         #    return (0, None, None)
 
         parser = BTSParser()
-        for line in page:
+        for line in page.splitlines():
             parser.feed(line)
         parser.close()
-        try:
-            page.fp._sock.recv = None
-        except:
-            pass
-        page.close()
 
         return parser.bugcount, parser.title, parser.hierarchy
 

reportbug/utils.py
@@ -1202,7 +1202,7 @@ def launch_mbox_reader(cmd, url, http_proxy, timeout):
         return
     (fd, fname) = TempFile()
     try:
-        for line in mbox:
+        for line in mbox.splitlines():
             fd.write(line)
         fd.close()
         if cmd is not None:

commit f3d4bfaabe833f753cb9033fd5e507180a9f280c

Add line endings when processing strings line by line
Bug introduced in d7e9eb8cc5d2efa4543bd35a806cbc36d7419e5e

reportbug/debbugs.py
@@ -971,7 +971,7 @@ def parse_html_report(number, url, http_proxy, timeout, followups=False, cgi=Tru
 
     parser = BTSParser(cgi=cgi, followups=followups)
     for line in page.splitlines():
-        parser.feed(line)
+        parser.feed(line + '\n')
     parser.close()
 
     items = parser.preblock
@@ -1123,7 +1123,7 @@ def get_reports(package, timeout, system='debian', mirrors=None, version=None,
 
         parser = BTSParser()
         for line in page.splitlines():
-            parser.feed(line)
+            parser.feed(line + '\n')
         parser.close()
 
         return parser.bugcount, parser.title, parser.hierarchy

reportbug/utils.py
@@ -1205,7 +1205,7 @@ def launch_mbox_reader(cmd, url, http_proxy, timeout):
     (fd, fname) = TempFile()
     try:
         for line in mbox.splitlines():
-            fd.write(line)
+            fd.write(line + '\n')
         fd.close()
         if cmd is not None:
             try: