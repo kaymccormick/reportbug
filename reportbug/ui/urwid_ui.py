0e303d0da902a5dcd655ddbac0119de745972194
Sandro Tosi <morph@debian.org>
2018-02-24 20:56:02 -0500

    Extend copyright years to 2018

--- reportbug/ui/urwid_ui.py
+++ reportbug/ui/urwid_ui.py
@@ -1,7 +1,7 @@
 # urwid user interface for reportbug
 #   Written by Chris Lawrence <lawrencc@debian.org>
 #   (C) 2006-08 Chris Lawrence
-#   Copyright (C) 2008-2017 Sandro Tosi <morph@debian.org>
+#   Copyright (C) 2008-2018 Sandro Tosi <morph@debian.org>
 #
 # This program is freely distributable per the following license:
 #
