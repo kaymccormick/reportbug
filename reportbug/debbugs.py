1c93364d2ab8280b330638fd1201ae6ee3176224
Sandro Tosi <morph@debian.org>
2018-05-13 00:40:00 -0400

    add contributors.debian.org to pseudo-packages list

--- reportbug/debbugs.py
+++ reportbug/debbugs.py
@@ -171,6 +171,7 @@ debother = {
     'cdimage.debian.org': 'CD Image issues',
     'cdrom': 'Installation system',
     'cloud.debian.org': 'Issues involving Debian images for public/private clouds',
+    'contributors.debian.org': 'Issues with the Debian Contributors Website and coordination of maintenance',
     'd-i.debian.org': 'Issues regarding the d-i.debian.org service and general Debian Installer tasks',
     'debian-maintainers': 'Problems and requests related to Debian Maintainers',
     'debian-i18n': 'Requests regarding Internationalization (i18n) of the distribution',
